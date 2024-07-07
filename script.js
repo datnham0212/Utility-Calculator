de693555ec2efde86717b3c995144e7402c8eec9
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-07-07 22:10:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var displayField = document.getElementsByClassName("calc-display");
-var count = 0;
+// var count = 0;
 function buttonPressed(that){
     
     var buttonValue = that.textContent;
@@ -10,17 +10,49 @@ function buttonPressed(that){
     }
     else if(buttonValue == "CE"){
         displayField[0].value = displayField[0].value.slice(0, -1);
-        count -= 1;
+        // count -= 1;
     }
-    else if(buttonValue == "0" && count == 0){
-        displayField[0].value = "";
+    // else if(buttonValue == "0" && count == 0){
+    //     displayField[0].value = "";
+    // }
+    
+    else if(buttonValue == "+"){
+        displayField[0].value += buttonValue;
+        operation();
+    }
+
+    else if(buttonValue == "="){
+        equals();
     }
 
     else{
         displayField[0].value += buttonValue;
-        count += 1
+        // count += 1
     }
-
-
     
+}
+
+function operation(){
+    if(displayField[0].value.includes('+')){
+        return '+'; 
+    }
+    else if(displayField[0].value.includes('-')){
+        return '-';
+    }
+    else if(displayField[0].value.includes('*')){
+        return '*';
+    }
+    else if(displayField[0].value.includes('/')){
+        return '/';
+    }
+}
+
+
+function equals(){
+    let str = String(displayField[0].value);
+    console.log(str.split(operation()));
+    let first = str.split(operation())[0];
+    let second = str.split(operation())[1];
+    let result = eval(first + operation() + second);   
+    displayField[0].value = result;
 }
