4a7ca90d03ad428a9a8744a1fc9772c39cf7b0cf
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-07-13 11:46:21 +0700

    Update conv-script.js

--- conv-script.js
+++ conv-script.js
@@ -3,41 +3,38 @@ function convertUnit(){
     var inputOption = document.getElementById("cdropdown1").value;
     var outputOption = document.getElementById("cdropdown2").value;
 
-    if(inputOption == "kilometers" && outputOption == "yards"){
-        var output = input * 1093.6133;
+    if (inputOption == "kilometers" && outputOption == "yards") {
+        output = input * 1093.6133;
     }
-    else if(outputOption == "kilometers" && inputOption == "yards"){
-        var output = input * 0.0009144;
+    else if (inputOption == "yards" && outputOption == "kilometers") {
+        output = input * 0.0009144;
     }
-    else if(inputOption == "miles" && outputOption == "kilometers"){
-        var output = input * 1.60934;
-    }else if(inputOption == "kilometers" && outputOption == "miles"){
-        var output = input * 0.621371;
-    }else if(inputOption == "miles" && outputOption == "meters"){
-        var output = input * 1609.34;
-    }else if(inputOption == "meters" && outputOption == "miles"){
-        var output = input * 0.000621371;
-    }else if(inputOption == "kilometers" && outputOption == "meters"){
-        var output = input * 1000;
-    }else if(inputOption == "meters" && outputOption == "kilometers"){
-        var output = input * 0.001;
-    }else if(inputOption == "kilometers" && outputOption == "feet"){
-        var output = input * 3280.84;
-    }else if(inputOption == "feet" && outputOption == "kilometers"){
-        var output = input * 0.000304
-    }
-    else if(inputOption == "feet" && outputOption == "meters"){
-        var output = input * 0.3048;
-    }
-    else if(inputOption == "meters" && outputOption == "feet"){
-        var output = input * 3.28084;
-    }
-    else if(inputOption == "feet" && outputOption == "yards"){
-        var output = input * 3;
-    }else if(inputOption == "yards" && outputOption == "feet"){
-        var output = input * 0.3333333;
-    }else if(inputOption == "yards" && outputOption == "meters"){
-        var output = input * 0.9144;
+    else if (inputOption == "miles" && outputOption == "kilometers") {
+        output = input * 1.60934;
+    } else if (inputOption == "kilometers" && outputOption == "miles") {
+        output = input * 0.621371;
+    } else if (inputOption == "miles" && outputOption == "meters") {
+        output = input * 1609.34;
+    } else if (inputOption == "meters" && outputOption == "miles") {
+        output = input * 0.000621371;
+    } else if (inputOption == "kilometers" && outputOption == "meters") {
+        output = input * 1000;
+    } else if (inputOption == "meters" && outputOption == "kilometers") {
+        output = input * 0.001;
+    } else if (inputOption == "kilometers" && outputOption == "feet") {
+        output = input * 3280.84;
+    } else if (inputOption == "feet" && outputOption == "kilometers") {
+        output = input * 0.0003048;
+    } else if (inputOption == "feet" && outputOption == "meters") {
+        output = input * 0.3048;
+    } else if (inputOption == "meters" && outputOption == "feet") {
+        output = input * 3.28084;
+    } else if (inputOption == "feet" && outputOption == "yards") {
+        output = input * 0.333333;
+    } else if (inputOption == "yards" && outputOption == "feet") {
+        output = input * 3;
+    } else if (inputOption == "yards" && outputOption == "meters") {
+        output = input * 0.9144;
     }
 
     document.getElementById("coutput").value = output;
@@ -47,3 +44,10 @@ function convertUnit(){
 // document.addEventListener('DOMContentLoaded', () =>{
 //     convertUnit();
 // });
+
+function swap(){
+    var from = document.getElementById("cdropdown1").value;
+    var to = document.getElementById("cdropdown2").value;
+    document.getElementById("cdropdown1").value = to;
+    document.getElementById("cdropdown2").value = from;
+}
